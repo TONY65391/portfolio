ba4256ac707be0d75843ac77fde8bd3177252ba2
TONY65391 <tonyanthony65391@gmail.com>
2025-11-18 10:36:36 -0600

    sixth commit

--- app/static/app.js
+++ app/static/app.js
@@ -3,4 +3,16 @@ const subMenu = document.querySelector('header ul');
 openBtn.onclick = () => {
     openBtn.classList.toggle('open');
     subMenu.classList.toggle('open');
-}
+};
+
+
+const toTop = document.querySelector('.top');
+
+window.addEventListener('load', () => {toTop.classList.add('shrink');});
+
+window.addEventListener('scroll', () => {if (document.documentElement.scrollTop === 0) {toTop.classList.add('shrink');} else{toTop.classList.remove('shrink');}});
+
+toTop.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
